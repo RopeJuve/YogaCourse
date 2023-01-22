fb5c724c0aa479488d55178f3fb233ba691d4012
RopeJuve <rope.s@hotmail.com>
2023-01-22 14:40:08 +0100

    header

--- src/components/Header/Card/CardtFull.jsx
+++ src/components/Header/Card/CardtFull.jsx
@@ -1,8 +1,21 @@
-
+import img from '../../../assets/young-beautiful-lady-sitting-near-window-make-yoga-exercises 1.png'
+import './Card.css'
 
 const CardtFull = () => {
   return (
-    <div>CardtFull</div>
+    <div className='cardFull__container'>
+      <div className='cardFull__text-container'>
+          <div className='text__title-container'>
+          <h3>Every-Day Open Master Classes.</h3>
+          <h1>3</h1>
+          </div>
+          <p>We’re boosting online yoga by enabling anyone in the world to learn from the best</p>
+          <a href='#'>READ MORE</a>
+      </div>
+      <div className='cadFull__img-container'>
+      <img src={img} alt="img" />
+      </div>
+    </div>
   )
 }
 
--- src/components/Header/Hero.jsx
+++ src/components/Header/Hero.jsx
@@ -1,21 +1,23 @@
 import "./Hero.css";
-import Card from './Card/Card'
-import CardFull from './Card/CardtFull'
+import Card from "./Card/Card";
+import CardFull from "./Card/CardtFull";
 import img from "../../assets/young-smiling-guy-sitting-fitness-mat-meditating-removebg 1.png";
-import img2 from '../../assets/man-standing-one-leg-full-shot (1).png'
-import img1 from '../../assets/man-standing-one-leg-full-shot.png'
+import img2 from "../../assets/man-standing-one-leg-full-shot (1).png";
+import img1 from "../../assets/man-standing-one-leg-full-shot.png";
 const Hero = () => {
   return (
     <div className="hero__container">
       <div className="hero__text-container">
-        <h1>Yoga to Release Stress</h1>
-        <p>
-          Yoga is a way of life, rather than a chore. Counteract the stresses of
-          modern life by becoming more mindful and compassionate.
-        </p>
-        <div className="hero__brtn-container">
-          <button className="get-started">Get Started</button>
-          <button className="learn-more">Learn More</button>
+        <div className="text-flex-column">
+          <h1>Yoga to Release Stress</h1>
+          <p>
+            Yoga is a way of life, rather than a chore. Counteract the stresses
+            of modern life by becoming more mindful and compassionate.
+          </p>
+          <div className="hero__brtn-container">
+            <button className="get-started">Get Started</button>
+            <button className="learn-more">Learn More</button>
+          </div>
         </div>
         <div className="hero__img-container">
           <div className="shape">
@@ -24,11 +26,13 @@ const Hero = () => {
         </div>
       </div>
       <div className="hero__card-container">
-          <div  className="card-flex">
-            <Card title='Make Your Own Plan For Yoga.' number='1' imgUrl={img1}/>
-            <Card title='Find a Yoga Mentor For You.' number='2' imgUrl={img2}/>
-          </div>
-            <CardFull />
+        <div className="card-flex">
+          <Card title="Make Your Own Plan For Yoga." number="1" imgUrl={img1} />
+          <Card title="Find a Yoga Mentor For You." number="2" imgUrl={img2} />
+        </div>
+        <div className="cardFull">
+          <CardFull />
+        </div>
       </div>
     </div>
   );
