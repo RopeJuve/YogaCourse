76860afb755263ff93abd5422b8f9d259df7b9dd
RopeJuve <rope.s@hotmail.com>
2023-01-30 00:58:40 +0100

    Courses Component

---
+++ src/components/Courses/CorseCard.jsx
@@ -0,0 +1,25 @@
+import './CourseCard.css';
+import img from '../../assets/Rating.png'
+
+const CorseCard = ({imgUrl, title, text}) => {
+  return (
+    <div className="course__container">
+      <div className="course__content-wrapper">
+        <img src={imgUrl} alt={imgUrl} />
+        <div className="course__content-text">
+          <h1>{title}</h1>
+          <p>{text}</p>
+        </div>
+        <div className="course__content-footer">
+          <div className="rating">
+            <img src={img} alt="img" />
+            <p>(18)</p>
+          </div>
+          <a href="#">Get Started</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CorseCard
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,9 +1,41 @@
-import './Courses.css'
+import "./Courses.css";
+import CourseCard from "./CorseCard";
+import course1 from "../../assets/Rectangle 4539.png";
+import course2 from "../../assets/Rectangle 4540.png";
+import course3 from "../../assets/Rectangle 4541.png";
 
 const Courses = () => {
   return (
-    <div>Courses</div>
-  )
-}
+    <div className="courses-container">
+      <div className="courses-wrapper">
+        <div className="content-head">
+          <h1>Popular Courses</h1>
+          <p>
+            Practice anywhere, anytime. Explore a new way to exercise and learn
+            more about yourself. We are providing the best.
+          </p>
+        </div>
+        <div className="card-wrapper">
+          <CourseCard
+            imgUrl={course1}
+            title="Resortive Yoga Training & Immersion"
+            text="Here is some tips for new job seekars who want to get a dream job and want to shine in his career."
+          />
+          <CourseCard
+            imgUrl={course2}
+            title="Resortive Yoga Training & Immersion"
+            text="Here is some tips for new job seekars who want to get a dream job and want to shine in his career."
+          />
+          <CourseCard
+            imgUrl={course3}
+            title="Resortive Yoga Training & Immersion"
+            text="Here is some tips for new job seekars who want to get a dream job and want to shine in his career."
+          />
+        </div>
+        <button>Browse All</button>
+      </div>
+    </div>
+  );
+};
 
-export default Courses
+export default Courses;
