9364f457244389302f4899ff024d1083c3494407
RopeJuve <rope.s@hotmail.com>
2023-01-29 21:48:07 +0100

    Features Component

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,9 +1,26 @@
-import './Features.css'
+import "./Features.css";
+import img from "../../assets/Group 5782.png";
 
 const Features = () => {
   return (
-    <div>Features</div>
-  )
-}
+    <div className="features-container">
+      <div className="features-wrapper">
+        <img src={img} alt="img" />
+        <div className="content-wraper">
+          <div className="content-header">
+            <h1>The Better Way to Start Yoga</h1>
+          </div>
+          <div className="content-text">
+            <p>
+              Practice anywhere, anytime. Explore a new way to exercise and
+              learn more about yourself. We are providing the best.
+            </p>
+            <button>Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Features
+export default Features;
