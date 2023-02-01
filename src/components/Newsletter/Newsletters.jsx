bb287576572f611e958847327b4ff82635b01196
RopeJuve <rope.s@hotmail.com>
2023-02-01 02:56:00 +0100

    Newsletters Component

--- src/components/Newsletter/Newsletters.jsx
+++ src/components/Newsletter/Newsletters.jsx
@@ -1,9 +1,22 @@
-import './Newsletters.css'
+import "./Newsletters.css";
+import email from '../../assets/envelope (1) 1.png'
 
 const Newsletters = () => {
   return (
-    <div>Newsletters</div>
-  )
-}
+    <div className="newaletter__container">
+      <div className="newsletter__wrapper">
+        <h1>Subscribe Our Newsletter</h1>
+        <p>Subscribe our newsletter for further updates about us</p>
+        <div className="newsletter__email-flex">
+          <div className="email-input">
+            <img src={email} alt='email' />
+            <input type="email" placeholder="Enter your email address" />
+          </div>
+          <button>Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Newsletters
+export default Newsletters;
