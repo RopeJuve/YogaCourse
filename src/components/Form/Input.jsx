680e585cc68af3961eb46ff7665130b2828214ec
Shterjov Robert <65367863+RopeJuve@users.noreply.github.com>
2023-02-16 04:30:44 +0100

    Form Validation (#3)

--- src/components/Form/Input.jsx
+++ src/components/Form/Input.jsx
@@ -1,13 +1,43 @@
-import './Input.css'
+import { useState, useRef } from "react";
+import "./Input.css";
 
-const Input = ({type, placeholder}) => {
-    const border = placeholder === '' ? 'border-none' : ''
+const Input = ({ type, name }) => {
+  const inputRef = useRef(null);
+  const [value, setValue] = useState("");
+  const [wrapper, setWrapper] = useState("");
+  const [error, setError] = useState("");
+  const border = name === "" ? "border-none" : "";
+  const none = name === "" ? "p-none" : "error";
+
+  const handelChange = (e) => {
+    setValue(e.target.value);
+
+    if (inputRef.current.checkValidity()) {
+      setWrapper("input__wrapper correct");
+    } else {
+      setWrapper("input__wrapper input-error");
+      setError(`You need to enter valid ${e.target.name}`);
+    }
+  };
 
   return (
-    <div className={`input__wrapper  ${border}`}>
-      <input type={type} placeholder={placeholder} />
-    </div>
+    <>
+      <div className={wrapper === "" ? ` input__wrapper ${border}` : wrapper}>
+        <label>{name}</label>
+        <input
+          ref={inputRef}
+          type={type}
+          name={name}
+          value={value}
+          onFocus={() => setWrapper("input__wrapper animate")}
+          onChange={handelChange}
+          onBlur={() => setWrapper("input__wrapper blur")}
+          required={name === "" ? false : true}
+        />
+      </div>
+      <p className={none}>{error}</p>
+    </>
   );
-}
+};
 
-export default Input
+export default Input;
